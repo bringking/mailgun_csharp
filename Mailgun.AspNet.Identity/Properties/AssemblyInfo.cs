8336f694b27a01ad9d3bfee3c71d3685441e6299
Charles King <bringking@gmail.com>
2014-11-16 16:35:26 -0700

    Updated versions and assembly information

--- Mailgun.AspNet.Identity/Properties/AssemblyInfo.cs
+++ Mailgun.AspNet.Identity/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mailgun.AspNet.Identity")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Mailgun API Wrapper- ASP.net Identity services")]
+[assembly: AssemblyDescription("An IIdentityMessageService implemention using the mailgun_csharp API wrapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Charles King")]
 [assembly: AssemblyProduct("Mailgun.AspNet.Identity")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright Charles King ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
