8767732016c93513cac1330143388d1ab18ddad1
Charles King <bringking@gmail.com>
2014-11-16 16:41:48 -0700

    Added an implementation of IMailgunMessageServiceOptions

--- Mailgun.AspNet.Identity/MailgunMessageServiceOptions.cs
+++ Mailgun.AspNet.Identity/MailgunMessageServiceOptions.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Mailgun.AspNet.Identity.Core;
 
 namespace Mailgun.AspNet.Identity
 {
-    public class MailgunMessageServiceOptions
+    public class MailgunMessageServiceOptions : IMailgunMessageServiceOptions
     {
+        public bool UseSsl { get; set; }
+        public string ApiKey { get; set; }
+        public string Domain { get; set; }
+        public bool UseHtmlBody { get; set; }
+        public bool UseDkim { get; set; }
+        public bool TestMode { get; set; }
+        public bool Tracking { get; set; }
+        public bool TrackingClicks { get; set; }
+        public bool TrackingOpen { get; set; }
+        public ICollection<string> DefaultTags { get; set; }
+        public IDictionary<string, string> DefaultHeaders { get; set; }
     }
 }
